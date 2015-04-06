60aaf9b85380d1f53206a4d3a11f574e8f77bdc9
Koen Punt <koen@koenpunt.nl>
2015-04-06 18:49:06 +0200

    update install script

--- .travis/install.sh
+++ .travis/install.sh
@@ -1,7 +1,7 @@
+export CFLAGS="-Wall -Wextra -Wdeclaration-after-statement -Wmissing-field-initializers -Wshadow -Wno-unused-parameter -ggdb3"
 phpize
 ./configure
 make all install
-EXTENSIONDIR=`php -r 'echo ini_get("extension_dir");'`
-echo "extension=${EXTENSIONDIR}/dotenv.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
-echo "extension=${EXTENSIONDIR}/dotenv.so" > /tmp/temp-php-config.ini
+echo "extension=dotenv.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
+echo "extension=dotenv.so" > /tmp/temp-php-config.ini
 cat /tmp/temp-php-config.ini
