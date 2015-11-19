531c4ca2a295140b9ff1c2565b6c7de0fe6a8e6b
Koen Punt <koen@koenpunt.nl>
2015-11-19 22:41:32 +0100

    enable dotenv flag

--- .travis/install.sh
+++ .travis/install.sh
@@ -1,6 +1,6 @@
 export CFLAGS="-Wall -Wextra -Wdeclaration-after-statement -Wmissing-field-initializers -Wshadow -Wno-unused-parameter -ggdb3"
 phpize
-./configure
+./configure --enable-dotenv
 make all install
 echo "extension=dotenv.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
 echo "extension=dotenv.so" > /tmp/temp-php-config.ini
